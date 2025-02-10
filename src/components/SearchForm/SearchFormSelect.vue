a0efeeaae1da98dd4f85f54d1b088c073e9ac362
Yan <1802614800@qq.com>
2025-02-10 23:47:50 +0800

    初次提交

---
+++ src/components/SearchForm/SearchFormSelect.vue
@@ -0,0 +1,41 @@
+<template>
+    <el-select
+        v-model="modelValue"
+        v-bind="$attrs"
+        v-on="$listeners"
+    >
+        <el-option
+            v-for="item in options"
+            :key="item.value"
+            :label="item.value"
+            :value="item.value"
+        />
+    </el-select>
+</template>
+<script>
+export default{
+    name:"SearchFormSelect",
+    model: {
+        event: "change"
+    },
+    props:{
+        value:{
+            type:[String,Number,Boolean]
+        },
+        options:{
+            type:Array,
+            default:()=>[]
+        }
+    },
+    computed:{
+        modelValue:{
+            get(){
+                return this.value
+            },
+            set(value){
+                // this.$emit('change',value)
+            }
+        }
+    }
+}
+</script>
---
+++ src/views/AboutView.vue
@@ -0,0 +1,57 @@
+<template>
+  <div class="about">
+    <h1>This is an about page</h1>
+    <div style="width: 100%;">
+      <SearchForm :fields="fields" />
+    </div>
+  </div>
+</template>
+<script>
+import SearchForm from "@/components/SearchForm";
+export default {
+  components: {
+    SearchForm,
+  },
+  data() {
+    return {
+      fields: [
+        {
+          key: "rule",
+          label: "规则名称",
+          component: "input",
+          componentProps: {
+            placeholder: "请输入规则名称",
+            maxlength: 30,
+          },
+          componentEvents: {
+            change(value) {
+              //规则的change事件
+              console.log(value,'修改了');
+              
+            },
+          },
+        },
+        {
+          key: "type",
+          label: "规则名称",
+          component: "select",
+          componentProps: {
+            placeholder: "请选择类型",
+            options: [
+              { label: "类型1", value: "type1" },
+              { label: "类型2", value: "type2" },
+            ],
+          },
+          componentEvents: {
+            change(value) {
+              //规则的change事件
+              console.log(value,'修改了');
+              
+            },
+          },
+        },
+      ],
+    };
+  },
+};
+</script>
